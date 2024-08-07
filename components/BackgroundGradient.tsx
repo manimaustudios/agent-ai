b2a0740c9ee567e3a540ba5bf598da771b531669
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-08-07 12:15:16 +0200

    basic setup

---
+++ components/BackgroundGradient.tsx
@@ -0,0 +1,9 @@
+const BackgroundGradient = () => {
+  return (
+    <div className="fixed left-0 top-0 -z-10 h-screen w-full">
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
+    </div>
+  );
+};
+
+export default BackgroundGradient;
