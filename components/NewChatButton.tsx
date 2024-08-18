d246cf0bdfcba719ec56ac9eef1120e74636e3a9
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-08-18 17:50:39 +0200

    NewChatButton

---
+++ components/NewChatButton.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { FaRegMessage } from "react-icons/fa6";
+import { Button } from "./ui/button";
+import { useChat } from "@/lib/providers/ChatProvider";
+
+type Props = {};
+
+function NewChatButton({}: Props) {
+  const { setChatHistory } = useChat();
+
+  const handleStartNewChat = () => {
+    setChatHistory([], "", "", false);
+  };
+  return (
+    <Button
+      className="flex items-center justify-center gap-2"
+      variant="ghost"
+      size="sm"
+      onClick={() => handleStartNewChat()}
+    >
+      New Chat
+      <FaRegMessage className="size-3" />
+    </Button>
+  );
+}
+
+export default NewChatButton;
