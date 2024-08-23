0621b38ffa1f731c2fb2679e7f7974a9376dce70
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-08-23 19:54:01 +0200

    sidebar update

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,34 +1,50 @@
-import { FaChevronLeft } from "react-icons/fa";
+"use client";
 
-import AccesButton from "./AccesButton";
+import { useState } from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
 import ChatHistory from "./ChatHistory";
 import NewChatButton from "./NewChatButton";
-import SubscriptionStatus from "./SubscriptionStatus";
 
 type SidebarProps = {
   isAuthenticated: boolean;
   userId: string | null;
+  children: React.ReactNode;
 };
 
-async function Sidebar({ isAuthenticated, userId }: SidebarProps) {
+function Sidebar({ isAuthenticated, userId, children }: SidebarProps) {
+  const [isOpen, setIsOpen] = useState(true);
   return (
-    <div className="flex h-screen w-56 flex-col bg-slate-900 p-2">
-      <div className="space-y-4">
-        <div className="ml-3 flex items-center justify-between">
-          Logo
-          <FaChevronLeft className="size-4" />
-        </div>
-        <NewChatButton />
+    <>
+      <div
+        className={`${isOpen ? "flex" : "hidden"} h-screen w-56 flex-col bg-slate-900 p-2`}
+      >
+        <div className="space-y-4">
+          <div className="ml-3 flex items-center justify-between">
+            Logo
+            <button
+              onClick={() => setIsOpen(false)}
+              className="-mr-2 flex size-6 items-center justify-center rounded-l-full bg-slate-800"
+            >
+              <FaChevronLeft className="size-4" />
+            </button>
+          </div>
+          <NewChatButton />
 
-        {isAuthenticated && <ChatHistory />}
+          {isAuthenticated && <ChatHistory />}
+        </div>
+        <div className="mt-auto space-y-2">
+          {/* server components */}
+          {children}
+        </div>
       </div>
-      <div className="mt-auto space-y-2">
-        {/* For testing */}
-        {/* <CreateUserButton userId={userId} /> */}
-        {isAuthenticated && <SubscriptionStatus userId={userId} />}
-        <AccesButton isAuth={isAuthenticated} />
-      </div>
-    </div>
+      <button
+        onClick={() => setIsOpen(true)}
+        className={`${!isOpen ? "flex" : "hidden"} absolute left-0 top-3 size-6 items-center justify-center rounded-r-full bg-slate-800`}
+      >
+        <FaChevronRight className="size-4" />
+      </button>
+    </>
   );
 }
 
