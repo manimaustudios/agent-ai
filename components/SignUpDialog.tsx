5906fe9dd8570dfe41893f6f73bc614b48a5907f
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-09-07 19:28:05 +0200

    SignUpDialog

---
+++ components/SignUpDialog.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { SignUp, useAuth } from "@clerk/nextjs";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { useEffect, useState } from "react";
+
+type SignUpDialogProps = {
+  buttonIcon?: React.ReactNode;
+};
+
+export function SignUpDialog({ buttonIcon }: SignUpDialogProps) {
+  const { isSignedIn, isLoaded } = useAuth();
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    const hash = window.location.hash;
+    const isCallback = hash.includes("sso-callback");
+
+    if (isCallback) {
+      setOpen(true);
+    } else if (isSignedIn) {
+      setOpen(false);
+    }
+  }, [isSignedIn, isLoaded]);
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button variant="outline" onClick={() => setOpen(true)}>
+          <div>{buttonIcon ? buttonIcon : "Sign Up"}</div>
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="w-auto justify-center">
+        <DialogHeader>
+          <DialogTitle className="mb-2 pt-4 text-center font-normal text-muted-foreground"></DialogTitle>
+          <SignUp
+            routing="hash"
+            signInUrl="/sign-in"
+            afterSignInUrl="/agent"
+            afterSignUpUrl="/agent"
+          />
+          <DialogDescription></DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
