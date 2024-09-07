b4c149732c934b76a4384430f427651923d55364
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-09-07 19:26:37 +0200

    AuthDialog update

--- components/AuthDialog.tsx
+++ components/AuthDialog.tsx
@@ -1,32 +1,56 @@
-import { SignIn } from "@clerk/nextjs";
+"use client";
+
+import { SignIn, useAuth } from "@clerk/nextjs";
 
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "./ui/dialog";
+import { useEffect, useState } from "react";
 
-export function AuthDialog() {
+type AuthDialogProps = {
+  buttonIcon?: React.ReactNode;
+};
+
+export function AuthDialog({ buttonIcon }: AuthDialogProps) {
+  const { isSignedIn, isLoaded } = useAuth();
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    const hash = window.location.hash;
+    const isCallback = hash.includes("sso-callback");
+
+    if (isCallback) {
+      setOpen(true);
+    } else if (isSignedIn) {
+      setOpen(false);
+    }
+  }, [isSignedIn, isLoaded]);
+
   return (
-    <Dialog>
-      <DialogTrigger>
-        <Button asChild>
-          <div>Sign In</div>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button onClick={() => setOpen(true)}>
+          <div>{buttonIcon ? buttonIcon : "Sign In"}</div>
         </Button>
       </DialogTrigger>
-      <DialogContent className="justify-center">
+      <DialogContent className="w-auto justify-center">
         <DialogHeader>
-          <DialogTitle className="mb-2 text-center font-normal text-muted-foreground">
-            You need to sign in first
-          </DialogTitle>
+          <DialogTitle className="mb-2 pt-4 text-center font-normal text-muted-foreground"></DialogTitle>
           <SignIn
             routing="hash"
             signUpUrl="/sign-up"
-            forceRedirectUrl="/agent"
+            afterSignInUrl="/agent"
+            afterSignUpUrl="/agent"
           />
+          <DialogDescription></DialogDescription>
         </DialogHeader>
       </DialogContent>
     </Dialog>
