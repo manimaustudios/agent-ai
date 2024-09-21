de85c76dd8d39dc86e8d7d0715058a27e9fb523b
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2024-09-21 09:37:11 +0200

    update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -28,5 +28,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "yearly",
       priority: 0.5,
     },
+    {
+      url: `${baseUrl}/disclaimer`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
   ];
 }
